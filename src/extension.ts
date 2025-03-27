4ba7eb550c35a1c79f00434e1e7296193f4c7dd8
Braith May <braith.may@wisetechglobal.com>
2025-03-28 10:58:32 +1100

    Play sound on error

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import Speaker from 'speaker';
+import * as wav from 'wav';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "vscodenarrator" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscodenarrator" is now active!');
+    let disposable = vscode.commands.registerCommand('vscodenarrator.StartNarrator', () => {
+        vscode.window.showInformationMessage('vscodeNarrator is now running!');
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscodenarrator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscodeNarrator!');
-	});
+    function playErrorAudio() {
+        const audioFilePath = path.join(context.extensionPath, 'assets', 'error-sound.wav');
 
-	context.subscriptions.push(disposable);
+        try {
+            // Create a wav file reader
+            const file = fs.createReadStream(audioFilePath);
+            const reader = new wav.Reader();
+
+            // Setup the speaker
+            const speaker = new Speaker({
+                channels: 2,  // Stereo
+                bitDepth: 16, // 16-bit
+                sampleRate: 44100 // Standard sample rate
+            });
+
+            // Pipe the file through the reader to the speaker
+            file.pipe(reader).pipe(speaker);
+
+            // reader.on('format', (format) => {
+            //     console.log('Audio format:', format);
+            // });
+
+            speaker.on('close', () => {
+                console.log('Audio playback completed');
+            });
+
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Audio playback error:', error.message);
+                vscode.window.showErrorMessage(`Failed to play audio: ${error.message}`);
+            } else {
+                console.error('Unknown audio playback error:', error);
+                vscode.window.showErrorMessage('Failed to play audio');
+            }
+        }
+    }
+
+    vscode.languages.onDidChangeDiagnostics(event => {
+        event.uris.forEach(uri => {
+            const diagnostics = vscode.languages.getDiagnostics(uri);
+            if (diagnostics.some(diagnostic => diagnostic.severity === vscode.DiagnosticSeverity.Error)) {
+                playErrorAudio();
+            }
+        });
+    });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
